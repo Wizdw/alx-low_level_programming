76e5e7486dc1eede88fb36d2a12f18ebdba32b1a
wizdw <wisdom700eduviere@gmail.com>
2022-12-14 15:31:58 -0800

    nested

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,25 +2,13 @@
 #include <unistdio.h>
 
 /**
- * main - Entry Point.use only user defined variable to create output.
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
  *
- * Description: Print notrebloh using predefined functions
- *
- * Return: 0
+ * return: on success 1
  */
 
-int main(void)
+int _putchar(char c)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
