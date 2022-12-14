ab71444a136ef646843c677a2e91c1c3c26dfe54
wizdw <wisdom700eduviere@gmail.com>
2022-12-14 15:13:34 -0800

    nested

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistdio.h>
+
+/**
+ * main - Entry Point.use only user defined variable to create output.
+ *
+ * Description: Print notrebloh using predefined functions
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
