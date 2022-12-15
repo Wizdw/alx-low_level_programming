41cb8ae5676839d7af68cf2cd1f68d8d71a2b73f
wizdw <wisdom700eduviere@gmail.com>
2022-12-14 17:39:30 -0800

    test

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
-int print_last_digits(int);
+int _abs(int r);
+int print_last_digit(int j);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
-void print_to_98(void n);
-int _putchar(char c);
+int add(int i, int l);
+void print_to_98(int n);
+void print_times_table(int n);
+
 #endif
